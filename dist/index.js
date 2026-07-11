9bf49dd01e09bed39de762653d993d880b0bf289
Adela Noponen <2370315+adetabs@users.noreply.github.com>
2026-07-11 19:18:48 +0300

    Updated build

--- dist/index.js
+++ dist/index.js
@@ -27604,6 +27604,11 @@ var debug = (err) => {
 };
 var debugJSON = (data) => debug(JSON.stringify(data, null, 2));
 var info = (message) => console.log(message);
+var error = (err) => {
+  console.error("ERROR:");
+  console.error(err);
+  (0, import_core2.setFailed)(getError(err));
+};
 var getError = (err) => {
   if (err instanceof Error) {
     return err.message;
@@ -27616,11 +27621,6 @@ var getError = (err) => {
   } catch {
     return "Unknown error";
   }
-};
-var error = (err) => {
-  console.error("ERROR:");
-  console.error(getError(err));
-  (0, import_core2.setFailed)(getError(err));
 };
 var getAutoMerge = (value) => {
   const autoMerge = value || DEFAULT_AUTO_MERGE;
